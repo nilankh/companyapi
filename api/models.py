b5922d9d1ee99fa6fa5fca160c67c9483757c38e
nilank nikhil <nilanknikhil@gmail.com>
2023-01-10 15:39:58 +0530

    nasic

--- api/models.py
+++ api/models.py
@@ -12,6 +12,23 @@ class Company(models.Model):
     added_date = models.DateTimeField(auto_now = True)
     active = models.BooleanField(default=True)
 
-
+    def __str__(self) :
+        return self.name + '---'+ self.location
 # Employee Model
 
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    address = models.CharField(max_length=250)
+    phone = models.CharField(max_length=10)
+    about = models.TextField()
+    position = models.CharField(max_length=50, choices=(
+        ('Manager','Manager'),
+        ('Software Developer', 'SDE'),
+        ('Project Lead', 'PL')
+    ))
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    
+
+    
--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
-from api.models import Company
+from api.models import Company, Employee
 
 # Create Searlizers
 class CompanySerializer(serializers.HyperlinkedModelSerializer):
     company_id=serializers.ReadOnlyField()
+
     class Meta:
         model= Company
         fields= "__all__"
+
+class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
+    id=serializers.ReadOnlyField()
+
+    class Meta:
+        model = Employee
+        fields = "__all__"
+    
+
+
